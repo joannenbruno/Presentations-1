46a1270afe8ca1ff044c639c9b3c3b2af1125a66
Joanne Bruno <jnbruno2015@gmail.com>
2016-12-08 00:50:08 -0600

    updated presentation slides with education

--- UISD Career Day 4-21-2016/gulpfile.js
+++ UISD Career Day 4-21-2016/gulpfile.js
@@ -4,10 +4,15 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');		// optimizing images
 var rename = require('gulp-rename');				// optimizing js
 var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
 
 // configuration object
 var config = {
+	proxyUrl: "http://localhost:8080",
+	browser: "chrome",
+	port: 7000,
 	paths: {
+		files: "./index.html",
 		images: "images/*"
 	}
 };
@@ -26,10 +31,23 @@ gulp.task('images', function() {
 	.pipe(gulp.dest(destConfig.paths.images));
 });
 
+// browser-sync task
+gulp.task('browser-sync', function() {
+	browserSync.init({
+        files: [config.paths.files],
+        browser: config.browser,
+        port: config.port,
+        server: {
+        	baseDir: './'
+        }
+	});
+});
+
 // watch task for any html/js changes
 gulp.task('watch', function() {
+	gulp.watch(config.paths.files, browserSync.reload);
 	gulp.watch(config.paths.images, ['images']);
 });
 
 // default gulp tasks
-gulp.task('default', ['watch'], function () {});
+gulp.task('default', ['browser-sync', 'watch'], function () {});
